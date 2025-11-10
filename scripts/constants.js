afd99f5289ecbbb46165000ef355f5eb26d5ac92
Michaela Robosova <robosovam@gmail.com>
2025-11-10 10:27:45 +0100

    Do not send support-dev notification
    when assignment request posted on an issue
    that is  assigned to someone else, and send
    bot reply in such a case.

--- scripts/constants.js
+++ scripts/constants.js
@@ -35,11 +35,14 @@ const ISSUE_LABEL_HELP_WANTED = 'help wanted';
 
 const BOT_MESSAGE_ISSUE_NOT_OPEN = `Hi! 👋 \n\n Thanks so much for your interest! **This issue is not open for contribution. Visit [Contributing guidelines](https://learningequality.org/contributing-to-our-open-code-base) to learn about the contributing process and how to find suitable issues.** \n\n We really appreciate your willingness to help—you're welcome to find a more suitable issue, and let us know if you have any questions. 😊`;
 
+const BOT_MESSAGE_ALREADY_ASSIGNED = `Hi! 👋 \n\n Thanks so much for your interest! **This issue is already assigned. Visit [Contributing guidelines](https://learningequality.org/contributing-to-our-open-code-base) to learn about the contributing process and how to find suitable issues.** \n\n We really appreciate your willingness to help—you're welcome to find a more suitable issue, and let us know if you have any questions. 😊`;
+
 module.exports = {
   LE_BOT_USERNAME,
   CLOSE_CONTRIBUTORS,
   KEYWORDS_DETECT_ASSIGNMENT_REQUEST,
   ISSUE_LABEL_HELP_WANTED,
   BOT_MESSAGE_ISSUE_NOT_OPEN,
+  BOT_MESSAGE_ALREADY_ASSIGNED,
   TEAMS_WITH_CLOSE_CONTRIBUTORS,
 };
--- scripts/contributor-issue-comment.js
+++ scripts/contributor-issue-comment.js
@@ -2,7 +2,8 @@ const {
   LE_BOT_USERNAME,
   KEYWORDS_DETECT_ASSIGNMENT_REQUEST,
   ISSUE_LABEL_HELP_WANTED,
-  BOT_MESSAGE_ISSUE_NOT_OPEN
+  BOT_MESSAGE_ISSUE_NOT_OPEN,
+  BOT_MESSAGE_ALREADY_ASSIGNED
 } = require('./constants');
 
 module.exports = async ({ github, context, core }) => {
@@ -10,6 +11,7 @@ module.exports = async ({ github, context, core }) => {
     const issueNumber = context.payload.issue.number;
     const issueUrl = context.payload.issue.html_url;
     const issueTitle = context.payload.issue.title;
+    const issueAssignee = context.payload.issue.assignee?.login;
     const escapedTitle = issueTitle.replace(/"/g, '\\"');
     const commentId = context.payload.comment.id;
     const commentTime = new Date(context.payload.comment.created_at);
@@ -25,6 +27,9 @@ module.exports = async ({ github, context, core }) => {
       .map(k => k.trim().toLowerCase())
       .filter(Boolean)
       .map(keyword => new RegExp(`\\b${keyword}\\b`, 'i'));
+    const isAssignmentRequest = keywordRegexes.find(regex => regex.test(commentBody));
+    const isIssueAssignedToSomeoneElse = issueAssignee && (issueAssignee !== commentAuthor);
+    const isHelpWanted = await hasLabel(ISSUE_LABEL_HELP_WANTED);
 
     async function hasLabel(name) {
       let labels = [];
@@ -57,25 +62,20 @@ module.exports = async ({ github, context, core }) => {
       }
     }
 
-    async function botReply() {
+    async function sendBotReply(message) {
       let response = null;
       try {
         response = await github.rest.issues.createComment({
           owner,
           repo,
           issue_number: issueNumber,
-          body: BOT_MESSAGE_ISSUE_NOT_OPEN
+          body: message
         });
-        if (response?.data?.html_url) {
-          core.setOutput('bot_replied', true);
-          const slackMessage = `*[${repo}] <${response.data.html_url}|Bot response sent> on issue: <${issueUrl}|${escapedTitle}>*`;
-          core.setOutput('slack_notification_bot_comment', slackMessage);
-        }
+        return response?.data?.html_url;
       } catch (error) {
         core.warning(`Failed to post bot comment: ${error.message}`);
-        core.setOutput('bot_replied', false);
+        return null;
       }
-      return response;
     }
 
     async function getIssues(assignee, state) {
@@ -92,7 +92,7 @@ module.exports = async ({ github, context, core }) => {
         return [];
       }
     }
-    
+
     async function getPullRequests(assignee, state) {
       try {
         const response = await github.rest.pulls.list({
@@ -107,7 +107,8 @@ module.exports = async ({ github, context, core }) => {
       }
     }
 
-    // Format information about author's assigned open issues as '(Issues #1 #2 | PRs #3)' and PRs for Slack message
+    // Format information about author's assigned open issues
+    // as '(Issues #1 #2 | PRs #3)' and PRs for Slack message
     function formatAuthorActivity(issues, pullRequests) {
       const parts = [];
       
@@ -128,41 +129,82 @@ module.exports = async ({ github, context, core }) => {
       return `(${parts.join(' | ')})`;
     }
 
-    const sendNotificationToSupportDevChannel = isCloseContributor || await hasLabel(ISSUE_LABEL_HELP_WANTED);
+    function shouldSendBotReply() {
+      if (isHelpWanted && isAssignmentRequest && isIssueAssignedToSomeoneElse) {
+        return [true, BOT_MESSAGE_ALREADY_ASSIGNED];
+      }
 
-    if (sendNotificationToSupportDevChannel) {
-      core.setOutput('webhook_url', supportDevSlackWebhookUrl);
-    } else {
-      // if we're not sending notification to #support-dev channel,
-      // send it to #support-dev-notifications and post GitHub bot reply if conditions are met
-      core.setOutput('webhook_url', supportDevNotificationsSlackWebhookUrl);
-      const matchedKeyword = keywordRegexes.find(regex => regex.test(commentBody));
-      // post a bot reply only if there is matched keyword and no previous bot comment in past hour
-      if (matchedKeyword) {
-        let recentBotComments = await findRecentCommentsByUser(LE_BOT_USERNAME);
-        if (recentBotComments.length > 0) {
-          core.setOutput('bot_replied', false);
-          core.setOutput('bot_reply_skipped', true);
-          const slackMessage = `*[${repo}] Bot response skipped on issue: <${issueUrl}|${escapedTitle}> (less than 1 hour since last bot reply)*`;
-          core.setOutput('slack_notification_bot_skipped', slackMessage);
-        } else {
-          console.log("bot is replying");
-          await botReply();
+      if (!isHelpWanted && isAssignmentRequest) {
+        return [true, BOT_MESSAGE_ISSUE_NOT_OPEN];
+      }
+
+      return [false, null];
+    }
+
+    function shouldContactSupport() {
+      // for close contributors, send notification
+      // to #support-dev under all circumstances
+      if (isCloseContributor) {
+        return true;
+      }
+
+      // for other contributors, do not send on non-help-wanted issues
+      if (!isHelpWanted) {
+        return false;
+      }
+
+      // on help-wanted issues, do not send if it's an assignment
+      // request and issue is already assigned to someone else
+      if (isAssignmentRequest && isIssueAssignedToSomeoneElse) {
+        return false;
+      }
+
+      return true;
+    }
+
+    const [shouldPostBot, botMessage] = shouldSendBotReply();
+    if (shouldPostBot) {
+      // post bot reply only when there are no previous
+      // bot comments in past hour to prevent overwhelming
+      // issue comment section
+      let recentBotComments = await findRecentCommentsByUser(LE_BOT_USERNAME);
+      if (recentBotComments.length > 0) {
+        const slackBotSkippedMessage = `*[${repo}] Bot response skipped on issue: <${issueUrl}|${escapedTitle}> (less than 1 hour since last bot reply)*`;
+
+        core.setOutput('bot_reply_skipped', true);
+        core.setOutput('slack_notification_bot_skipped', slackBotSkippedMessage);
+      } else {
+        const botReplyUrl = await sendBotReply(botMessage);
+        if (botReplyUrl) {
+          const slackMessage = `*[${repo}] <${botReplyUrl}|Bot response sent> on issue: <${issueUrl}|${escapedTitle}>*`;
+          core.setOutput('bot_replied', true);
+          core.setOutput('slack_notification_bot_comment', slackMessage);
         }
       }
     }
 
-    let message = `*[${repo}] <${issueUrl}#issuecomment-${commentId}|New comment> on issue: <${issueUrl}|${escapedTitle}> by _${commentAuthor}_*`;
+    const contactSupport = shouldContactSupport();
+    let slackMessage = `*[${repo}] <${issueUrl}#issuecomment-${commentId}|New comment> on issue: <${issueUrl}|${escapedTitle}> by _${commentAuthor}_*`;
 
-    if (sendNotificationToSupportDevChannel) {
+    if (contactSupport) {
+      // Append activity info when sending to #support-dev
+      // to guide the decision on whether to assign an issue
       const [assignedOpenIssues, openPRs] = await Promise.all([
         getIssues(commentAuthor, 'open'),
         getPullRequests(commentAuthor, 'open')
       ]);
       const authorActivity = formatAuthorActivity(assignedOpenIssues, openPRs);
-      message += ` _${authorActivity}_`;
+      slackMessage += ` _${authorActivity}_`;
+
+      core.setOutput('webhook_url', supportDevSlackWebhookUrl);
+      core.setOutput('slack_notification_comment', slackMessage);
+    } else {
+      // if we're not sending notification to #support-dev,
+      // send it to #support-dev-notifications and if the comment
+      // appears to be an assignment request, post GitHub bot reply
+      core.setOutput('webhook_url', supportDevNotificationsSlackWebhookUrl);
+      core.setOutput('slack_notification_comment', slackMessage);
     }
-    core.setOutput('slack_notification_comment', message);
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);
   }
