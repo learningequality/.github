4696bc7b40fc4e1dc6f18d5f5ad30bfdfc16f48a
Michaela Robosova <robosovam@gmail.com>
2025-11-10 10:27:42 +0100

    Improve naming + lint

--- scripts/contributor-issue-comment.js
+++ scripts/contributor-issue-comment.js
@@ -26,58 +26,56 @@ module.exports = async ({ github, context, core }) => {
       .filter(Boolean)
       .map(keyword => new RegExp(`\\b${keyword}\\b`, 'i'));
 
-
-
     async function hasLabel(name) {
-        let labels = [];
-        try {
-          const response = await github.rest.issues.listLabelsOnIssue({
-            owner,
-            repo,
-            issue_number: issueNumber
-          });
-          labels = response.data.map(label => label.name);
-        } catch (error) {
-           core.warning(`Failed to fetch labels on issue #${issueNumber}: ${error.message}`);
-           labels = [];
-        }
-        return labels.some(label => label.toLowerCase() === name.toLowerCase());
+      let labels = [];
+      try {
+        const response = await github.rest.issues.listLabelsOnIssue({
+          owner,
+          repo,
+          issue_number: issueNumber
+        });
+        labels = response.data.map(label => label.name);
+      } catch (error) {
+        core.warning(`Failed to fetch labels on issue #${issueNumber}: ${error.message}`);
+        labels = [];
+      }
+      return labels.some(label => label.toLowerCase() === name.toLowerCase());
     }
 
     async function findRecentCommentsByUser(username) {
-      try{
-          let response = await github.rest.issues.listComments({
-              owner,
-              repo,
-              issue_number: issueNumber,
-              since: oneHourBefore.toISOString()
-          });
-          return response.data.filter(comment => comment.user.login === username);
+      try {
+        let response = await github.rest.issues.listComments({
+          owner,
+          repo,
+          issue_number: issueNumber,
+          since: oneHourBefore.toISOString()
+        });
+        return response.data.filter(comment => comment.user.login === username);
       } catch (error) {
-          core.warning(`Failed to fetch comments on issue #${issueNumber}: ${error.message}`);
-          return [];
+        core.warning(`Failed to fetch comments on issue #${issueNumber}: ${error.message}`);
+        return [];
       }
     }
 
-    async function botReply(){
-        let response = null;
-        try {
-            response = await github.rest.issues.createComment({
-              owner,
-              repo,
-              issue_number: issueNumber,
-              body: BOT_MESSAGE_ISSUE_NOT_OPEN
-            });
-            if (response?.data?.html_url) {
-              core.setOutput('bot_replied', true);
-              const slackMessage = `*[${repo}] <${response.data.html_url}|Bot response sent> on issue: <${issueUrl}|${escapedTitle}>*`;
-              core.setOutput('slack_notification_bot_comment', slackMessage);
-            }
-        } catch (error) {
-            core.warning(`Failed to post bot comment: ${error.message}`);
-            core.setOutput('bot_replied', false);
+    async function botReply() {
+      let response = null;
+      try {
+        response = await github.rest.issues.createComment({
+          owner,
+          repo,
+          issue_number: issueNumber,
+          body: BOT_MESSAGE_ISSUE_NOT_OPEN
+        });
+        if (response?.data?.html_url) {
+          core.setOutput('bot_replied', true);
+          const slackMessage = `*[${repo}] <${response.data.html_url}|Bot response sent> on issue: <${issueUrl}|${escapedTitle}>*`;
+          core.setOutput('slack_notification_bot_comment', slackMessage);
         }
-        return response;
+      } catch (error) {
+        core.warning(`Failed to post bot comment: ${error.message}`);
+        core.setOutput('bot_replied', false);
+      }
+      return response;
     }
 
     async function getIssues(assignee, state) {
@@ -94,7 +92,7 @@ module.exports = async ({ github, context, core }) => {
         return [];
       }
     }
-
+    
     async function getPullRequests(assignee, state) {
       try {
         const response = await github.rest.pulls.list({
@@ -109,7 +107,7 @@ module.exports = async ({ github, context, core }) => {
       }
     }
 
-    // Format information about author's assigned open issues as '(Issues #1 #2 | PRs #3)' and PRs for Slack message 
+    // Format information about author's assigned open issues as '(Issues #1 #2 | PRs #3)' and PRs for Slack message
     function formatAuthorActivity(issues, pullRequests) {
       const parts = [];
       
@@ -140,19 +138,17 @@ module.exports = async ({ github, context, core }) => {
       core.setOutput('webhook_url', supportDevNotificationsSlackWebhookUrl);
       const matchedKeyword = keywordRegexes.find(regex => regex.test(commentBody));
       // post a bot reply only if there is matched keyword and no previous bot comment in past hour
-      if(matchedKeyword){
-        let lastBotComment;
-        let PastBotComments = await findRecentCommentsByUser(LE_BOT_USERNAME);
-        if(PastBotComments.length > 0){
-                lastBotComment = PastBotComments.at(-1);
-                core.setOutput('bot_replied', false);
-                core.setOutput('bot_reply_skipped', true);
-                const slackMessage = `*[${repo}] Bot response skipped on issue: <${issueUrl}|${escapedTitle}> (less than 1 hour since last bot reply)*`;
-                core.setOutput('slack_notification_bot_skipped', slackMessage);
-            } else if(PastBotComments.length === 0){
-                console.log("bot is replying");
-                lastBotComment = await botReply();
-            }
+      if (matchedKeyword) {
+        let recentBotComments = await findRecentCommentsByUser(LE_BOT_USERNAME);
+        if (recentBotComments.length > 0) {
+          core.setOutput('bot_replied', false);
+          core.setOutput('bot_reply_skipped', true);
+          const slackMessage = `*[${repo}] Bot response skipped on issue: <${issueUrl}|${escapedTitle}> (less than 1 hour since last bot reply)*`;
+          core.setOutput('slack_notification_bot_skipped', slackMessage);
+        } else {
+          console.log("bot is replying");
+          await botReply();
+        }
       }
     }
 
